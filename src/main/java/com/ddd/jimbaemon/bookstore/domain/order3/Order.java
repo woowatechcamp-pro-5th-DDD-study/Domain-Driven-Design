0f4656e117c07c886af2552a9aefe8dc8407e787
jimbaemon <jimbae@kakao.com>
2023-02-09 01:55:05 +0900

    feat: 1.6 엔티티와 밸류

---
+++ src/main/java/com/ddd/jimbaemon/bookstore/domain/order3/Order.java
@@ -0,0 +1,36 @@
+package com.ddd.jimbaemon.bookstore.domain.order3;
+
+import java.util.Objects;
+
+public class Order {
+
+    private String orderNumber; //특정한 규칙에 따라 생성한 주문 번호
+    private Money totalAmounts;
+    private ShippingInfo shippingInfo;
+    private OrderState state;
+
+    public Order(String orderNumber, Money totalAmounts,
+        ShippingInfo shippingInfo, OrderState state) {
+        this.orderNumber = orderNumber;
+        this.totalAmounts = totalAmounts;
+        this.shippingInfo = shippingInfo;
+        this.state = state;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Order order = (Order) o;
+        return Objects.equals(orderNumber, order.orderNumber); // 엔티티의 식별자가 같으면 두 엔티티는 같다고 판단할 수 있다.
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderNumber);
+    }
+}
---
+++ src/main/java/com/ddd/jimbaemon/bookstore/domain/order3/OrderLine.java
@@ -0,0 +1,19 @@
+package com.ddd.jimbaemon.bookstore.domain.order3;
+
+import com.ddd.jimbaemon.bookstore.domain.order2.Product;
+
+public class OrderLine {
+
+    private Product product; //주문 상품
+    private Money price; //상품 가격 (돈이라는 의미가 명확해진다)
+    private int quantity; //구매 개수
+    private Money amounts; //구매 가격 합 (돈이라는 의미가 명확해진다)
+
+    public OrderLine(Product product, Money price, int quantity) {
+        this.product = product;
+        this.price = price;
+        this.quantity = quantity;
+        this.amounts = price.multiply(quantity);
+    }
+
+}
